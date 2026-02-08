e15bc49d1ea531f8bd4b3f4d69a7037cb650967a
wexiumx <lukas.bludnickas@proton.me>
2026-02-08 15:47:38 +0000

    added some text and nvim in to install.sh

--- install.sh
+++ install.sh
@@ -6,53 +6,52 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 info_message() {
-	echo -e "${PURPLE}$1${NC}"
+  echo -e "${PURPLE}$1${NC}"
 }
 
 success_message() {
-	echo -e "${GREEN}$1${NC}"
+  echo -e "${GREEN}$1${NC}"
 }
 
 error_message() {
-	echo -e "${RED}$1${NC}"
+  echo -e "${RED}$1${NC}"
 }
 
 installPackageByYay() {
   local pkg=$1
-    if [[ $(pacman -Q "${pkg}" 2>/dev/null) ]]; then
-      success_message "${pkg} is already installed"
-    else
-    	yay -S --needed --noconfirm --answerclean All --answerdiff None "${pkg}"
-	fi
+  if [[ $(pacman -Q "${pkg}" 2>/dev/null) ]]; then
+    success_message "${pkg} is already installed"
+  else
+    yay -S --needed --noconfirm --answerclean All --answerdiff None "${pkg}"
+  fi
 }
 
 installPackageByPacman() {
-	local pkg=$1
-    if [[ $(pacman -Q "${pkg}" 2>/dev/null) ]]; then
-      success_message "${pkg} is already installed"
-    else
-		sudo pacman -S --noconfirm "${pkg}" 
-	fi
+  local pkg=$1
+  if [[ $(pacman -Q "${pkg}" 2>/dev/null) ]]; then
+    success_message "${pkg} is already installed"
+  else
+    sudo pacman -S --noconfirm "${pkg}"
+  fi
 }
 
-
 installYay() {
-		info_message "Installing yay..."
+  info_message "Installing yay..."
 
-		if command -v yay > /dev/null; then
-  			success_message "yay is installed. Skipping installation"
-		else
-  			error_message "yay is not installed. Installing..."
-  			sleep 1
-  			sudo pacman -S --needed --noconfirm base-devel less
-  			whereami=$(pwd)
-  			git clone https://aur.archlinux.org/yay.git ~/Downloads/yay
- 	 		cd ~/Downloads/yay
- 	 		makepkg -si
-  			cd $whereami
-  			rm -rf ~/Downloads/yay
-  			success_message "yay has been installed successfully"
-		fi
+  if command -v yay >/dev/null; then
+    success_message "yay is installed. Skipping installation"
+  else
+    error_message "yay is not installed. Installing..."
+    sleep 1
+    sudo pacman -S --needed --noconfirm base-devel less
+    whereami=$(pwd)
+    git clone https://aur.archlinux.org/yay.git ~/Downloads/yay
+    cd ~/Downloads/yay
+    makepkg -si
+    cd $whereami
+    rm -rf ~/Downloads/yay
+    success_message "yay has been installed successfully"
+  fi
 }
 
 clear
@@ -61,56 +60,56 @@ sleep 1
 clear
 
 info_message "Installing Ghostty terminal..."
-sleep 3
+sleep 1
 clear
 
 installPackageByPacman ghostty
-sleep 1
 clear
 
 info_message "Installing zsh..."
-sleep 3
-clear
-
-installPackageByPacman zsh
 sleep 1
 clear
 
+installPackageByPacman zsh
+clear
 
 info_message "changing shell to zsh..."
-sleep 3
+sleep 1
 clear
 
 chsh -s /bin/zsh
 success_message "Your shell is successfully set to zsh"
-sleep 5
 clear
 
-
-
 info_message "Installing oh my posh (prompt)..."
-sleep 3
+sleep 1
 clear
 
 installPackageByYay oh-my-posh
-sleep 1
 clear
 
 info_message "Installing stow..."
-sleep 3
-clear
-
-installPackageByPacman stow
 sleep 1
 clear
 
-info_message "Applying stow..."
-sleep 3
-clear
-stow .	
-clear
-success_message "Stow applied successfully"
+info_message "Installing Nvim"
+sleep 1
 clear
 
-success_message "Done, enjoy my terminal :)"
+installPackageByPacman nvim
+clear
+
+installPackageByPacman stow
+clear
+
+info_message "Applying stow..."
+sleep 1
+clear
+stow .
+clear
+success_message "Stow applied successfully"
+sleep 1
+clear
+
+success_message "Done, enjoy my terminal :), don't forget to reboot!"
 sleep 5
