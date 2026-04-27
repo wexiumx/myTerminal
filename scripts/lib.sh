21194d5dcad184cb569030365fa7333183a5ad06
Lukas Bludnickas <lukas.bludnickas@proton.me>
2026-04-27 12:41:12 +0500

    Update lib.sh

---
+++ scripts/lib.sh
@@ -0,0 +1,70 @@
+# ----------------------------------------------------------
+# Define message colors
+# ----------------------------------------------------------
+
+PURPLE='\033[0;35m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# ----------------------------------------------------------
+# Colored messages
+# ----------------------------------------------------------
+
+info_message() {
+  echo -e "${PURPLE}$1${NC}"
+}
+
+success_message() {
+  echo -e "${GREEN}$1${NC}"
+}
+
+error_message() {
+  echo -e "${RED}$1${NC}"
+}
+
+# ----------------------------------------------------------
+# Welcome message
+# ----------------------------------------------------------
+
+welcome_message() {
+  clear
+  echo -e "${PURPLE}"
+  echo -e "\t  _______                  _             _  "
+  echo -e "\t |__   __|                (_)           | | "
+  echo -e "\t    | | ___ _ __ _ __ ___  _ _ __   __ _| | "
+  echo -e "\t    | |/ _ \ '__| '_ ` _ \| | '_ \ / _` | | "
+  echo -e "\t    | |  __/ |  | | | | | | | | | | (_| | | "
+  echo -e "\t    |_|\___|_|  |_| |_| |_|_|_| |_|\__,_|_| "
+  echo -e "\t                                          \n"
+                                           
+
+  echo -e "\t      Welcome to my terminal installer :)"
+  echo -e "${NC}\n"
+}
+
+# ----------------------------------------------------------
+# Finish setup
+# ----------------------------------------------------------
+
+finish_setup() {
+  notify-send "Welcome, $USER" "Thank you for downloading my terminal :)"
+  info_message "Changing shell to zsh"
+  chsh -s /bin/zsh
+  success_message "Finished customizations. Restart the machine to enjoy your new terminal"
+  exit 0
+}
+
+# ----------------------------------------------------------
+# Install packages
+# ----------------------------------------------------------
+
+installPackages() {
+  for pkg; do
+    if [[ $(pacman -Q "${pkg}" 2>/dev/null) ]]; then
+      success_message "${pkg} is already installed"
+      continue
+    fi
+    yay -S --needed --noconfirm --answerclean All --answerdiff None "${pkg}"
+  done
+}
