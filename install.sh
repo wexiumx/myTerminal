e8fb27004e6f2eaa9964e6b21e32f44e7fdd7c84
wexiumx <lukas.bludnickas@proton.me>
2026-04-27 19:05:55 +0300

    adding btop to stow loop

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 set -e
 trap 'error_message "Script failed at line $LINENO"' ERR
 
-
 # ----------------------------------------------------------
 # Load the library and packages list
 # ----------------------------------------------------------
@@ -20,28 +19,26 @@ welcome_message
 sleep 2
 clear
 
-
 # --------------------------------------------------------------------
 # Ask if the user wants to do a backup of the current .config folder
 # --------------------------------------------------------------------
 
 info_message "Do you want to backup your current .config directory? (y/n, default: y): "
 read -r backup_choice
-backup_choice=${backup_choice:-y}  # Default to 'y' if empty
+backup_choice=${backup_choice:-y} # Default to 'y' if empty
 
 if [[ "$backup_choice" == "y" ]]; then
-    backup_dir="$HOME/.config/terminal_backup"
-    mkdir -p "$backup_dir"
+  backup_dir="$HOME/.config/terminal_backup"
+  mkdir -p "$backup_dir"
 
-    for dir in fastfetch ghostty nvim ohmyposh; do
-        if [ -d "$HOME/.config/$dir" ]; then
-            mv "$HOME/.config/$dir" "$backup_dir/"
-        fi
-    done
+  for dir in fastfetch ghostty nvim ohmyposh; do
+    if [ -d "$HOME/.config/$dir" ]; then
+      mv "$HOME/.config/$dir" "$backup_dir/"
+    fi
+  done
 
-    success_message "Backup created at $backup_dir"
+  success_message "Backup created at $backup_dir"
 fi
-
 
 # ----------------------------------------------------------
 # Create required folders
@@ -59,7 +56,6 @@ sleep 1
 
 clear
 
-
 # ----------------------------------------------------------
 # Check if yay is installed. If not, installs it
 # ----------------------------------------------------------
@@ -68,7 +64,6 @@ installYay
 
 sleep 1
 clear
-
 
 # ----------------------------------------------------------
 # Install packages
@@ -89,12 +84,11 @@ clear
 info_message "applying stow"
 sleep 1
 
-for dir in fastfetch ghostty nvim ohmyposh zsh; do
-	stow "$dir"
+for dir in fastfetch ghostty nvim ohmyposh zsh btop; do
+  stow "$dir"
 done
 
 clear
-
 
 # ----------------------------------------------------------
 # final message
