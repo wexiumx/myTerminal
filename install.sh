0aa8188c1900594b21ac4f2a68e44d0a0223bb90
Lukas Bludnickas <lukas.bludnickas@proton.me>
2026-04-27 13:53:16 +0500

    adding backup and package installation

--- install.sh
+++ install.sh
@@ -18,14 +18,7 @@ sleep 2
 clear
 
 
-# ----------------------------------------------------------
-# Create required folders
-# ----------------------------------------------------------
 
-mkdir -p ~/.config/fasfetch
-mkdir -p ~/.config/ghostty
-mkdir -p ~/.config/nvim
-mkdir -p ~/.config/ohmyposh
 
 # --------------------------------------------------------------------
 # Ask if the user wants to do a backup of the current .config folder
@@ -34,15 +27,61 @@ mkdir -p ~/.config/ohmyposh
 info_message "Do you want to backup your current .config directory? (y/n, default: y): "
 read backup_choice
 backup_choice=${backup_choice:-y}  # Default to 'y' if empty
+
 if [[ "$backup_choice" == "y" ]]; then
-	mkdir -p ~/.config/terminal_backup
-	mv ~/.config/fastfetch ~/.config/terminal_backup
-	mv ~/.config/ghostty ~/.config/terminal_backup
-	mv ~/.config/nvim ~/.config/terminal_backup
-	mv ~/.config/ohmyposh ~/.config/terminal_backup
-	success_message "Backup of .config created at ~/config_backup"
+    backup_dir="$HOME/.config/terminal_backup"
+    mkdir -p "$backup_dir"
+
+    for dir in fastfetch ghostty nvim ohmyposh; do
+        if [ -d "$HOME/.config/$dir" ]; then
+            mv "$HOME/.config/$dir" "$backup_dir/"
+        fi
+    done
+
+    success_message "Backup created at $backup_dir"
 fi
 
+
+# ----------------------------------------------------------
+# Create required folders
+# ----------------------------------------------------------
+
+mkdir -p ~/.config/fastfetch
+mkdir -p ~/.config/ghostty
+mkdir -p ~/.config/nvim
+mkdir -p ~/.config/ohmyposh
+
+
+
+# ----------------------------------------------------------
+# Check if yay is installed. If not, installs it
+# ----------------------------------------------------------
+
+if command -v yay > /dev/null; then
+  success_message "yay is installed. Skipping installation"
+else
+  error_message "yay is not installed. Installing..."
+  sleep 1
+  sudo pacman -S --needed --noconfirm base-devel less
+  whereami=$(pwd)
+  git clone https://aur.archlinux.org/yay.git ~/Downloads/yay
+  cd ~/Downloads/yay
+  makepkg -si
+  cd $whereami
+  rm -rf ~/Downloads/yay
+  success_message "yay has been installed successfully"
+fi
+
+sleep 1
+clear
+
+
+# ----------------------------------------------------------
+# Install packages
+# ----------------------------------------------------------
+
+installPackages "${shell[@]}"
+installPackages "${tools[@]}"
 
 
 
